97f44adffa24509cdfca425fc136e8b9e339acb6
Julia Ivanova <jeivano91@gmail.com>
2020-06-23 14:19:18 +0300

    fix and add bem

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,49 @@
 let dropdowns = document.querySelectorAll('.navbar .dropdown-toggler');
 let dropdownIsOpen = false;
+let mouseMoving = true;
+
+function dropdownHandler(e) {
+  let target = document.querySelector('#' + e.target.dataset.dropdown);
+
+  if (e.type === 'mousemove') {
+      mouseMoving = false;
+  }
+
+  if (e.type === 'mouseover' && mouseMoving === false) {
+
+      if (target.classList.contains('show')) {
+        setTimeout(() => {
+          target.classList.remove('show');
+          dropdownIsOpen = false;
+        }, 500);
+      } else {
+          target.classList.add('show');
+          dropdownIsOpen = true;
+      }
+
+  } else if (e.type === 'mouseout') {
+
+    if (target.classList.contains('show')) {
+      setTimeout(() => {
+      target.classList.remove('show');
+      dropdownIsOpen = false;
+      }, 500);
+    } else {
+        target.classList.add('show');
+        dropdownIsOpen = true;
+    }
+  }
+}
 
 // Handle dropdown menu
 if (dropdowns.length) {
   dropdowns.forEach((dropdown) => {
-    dropdown.addEventListener('click', (event) => {
-      let target = document.querySelector('#' + event.target.dataset.dropdown);
-
-      if (target) {
-        if (target.classList.contains('show')) {
-          target.classList.remove('show');
-          dropdownIsOpen = false;
-        } else {
-          target.classList.add('show');
-          dropdownIsOpen = true;
-        }
-      }
-    })
-  })
+    dropdown.onmouseover = dropdown.onmouseout = dropdown.onmousemove = dropdownHandler;
+  });
 }
 
 // Handle closing dropdowns if a user clicked the body
-window.addEventListener('mouseup', (event) => {
+window.addEventListener('click', (event) => {
   if (dropdownIsOpen) {
     dropdowns.forEach((dropdownButton) => {
       let dropdown = document.querySelector('#' + dropdownButton.dataset.dropdown);
@@ -39,7 +61,7 @@ window.addEventListener('mouseup', (event) => {
 });
 
 let navbarMenu = document.querySelector('.navbar-menu');
-let buttonToggler = document.querySelector('.navbar-toggler');
+let buttonToggler = document.querySelector('.navbar-header__navbar-toggler');
     buttonToggler.addEventListener('click', () => {
       navbarMenu.classList.toggle("nav-active");
     });
--- build/javascript/index.js
+++ build/javascript/index.js
@@ -2,28 +2,49 @@
 
 var dropdowns = document.querySelectorAll('.navbar .dropdown-toggler');
 var dropdownIsOpen = false;
+var mouseMoving = true;
+
+function dropdownHandler(e) {
+  var target = document.querySelector('#' + e.target.dataset.dropdown);
+
+  if (e.type === 'mousemove') {
+    mouseMoving = false;
+  }
+
+  if (e.type === 'mouseover' && mouseMoving === false) {
+
+    if (target.classList.contains('show')) {
+      setTimeout(function () {
+        target.classList.remove('show');
+        dropdownIsOpen = false;
+      }, 500);
+    } else {
+      target.classList.add('show');
+      dropdownIsOpen = true;
+    }
+  } else if (e.type === 'mouseout') {
+
+    if (target.classList.contains('show')) {
+      setTimeout(function () {
+        target.classList.remove('show');
+        dropdownIsOpen = false;
+      }, 500);
+    } else {
+      target.classList.add('show');
+      dropdownIsOpen = true;
+    }
+  }
+}
 
 // Handle dropdown menu
 if (dropdowns.length) {
   dropdowns.forEach(function (dropdown) {
-    dropdown.addEventListener('click', function (event) {
-      var target = document.querySelector('#' + event.target.dataset.dropdown);
-
-      if (target) {
-        if (target.classList.contains('show')) {
-          target.classList.remove('show');
-          dropdownIsOpen = false;
-        } else {
-          target.classList.add('show');
-          dropdownIsOpen = true;
-        }
-      }
-    });
+    dropdown.onmouseover = dropdown.onmouseout = dropdown.onmousemove = dropdownHandler;
   });
 }
 
 // Handle closing dropdowns if a user clicked the body
-window.addEventListener('mouseup', function (event) {
+window.addEventListener('click', function (event) {
   if (dropdownIsOpen) {
     dropdowns.forEach(function (dropdownButton) {
       var dropdown = document.querySelector('#' + dropdownButton.dataset.dropdown);
@@ -41,7 +62,7 @@ window.addEventListener('mouseup', function (event) {
 });
 
 var navbarMenu = document.querySelector('.navbar-menu');
-var buttonToggler = document.querySelector('.navbar-toggler');
+var buttonToggler = document.querySelector('.navbar-header__navbar-toggler');
 buttonToggler.addEventListener('click', function () {
   navbarMenu.classList.toggle("nav-active");
 });
